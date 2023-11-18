a160b3d12130c06851e7e0938e81473a0c8a1c38
subhashishjungshah <subhashishjungshah@gmail.com>
2023-11-18 22:09:53 +0545

    added more designs interms of ui prespective

--- Client/components/Navbar.tsx
+++ Client/components/Navbar.tsx
@@ -5,16 +5,20 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 import { useContext, useEffect, useState } from "react";
 import { Web3Context } from "@context/Web3context";
+import Avatar from "react-avatar";
 
 const NavBar = () => {
   const { account, requestAccount, logout } = useContext(Web3Context);
   const [title, setTitle] = useState<string>("");
+  const [showProfile, setShowProfile] = useState<boolean>(false);
 
   useEffect(() => {
     if (account) {
       setTitle("Disconnect wallet");
+      setShowProfile(true);
     } else {
       setTitle("Connect wallet");
+      setShowProfile(false);
     }
   }, [account]);
 
@@ -38,11 +42,26 @@ const NavBar = () => {
             containerStyles="text-primary-blue rounded-full bg-white min-w-[130px] border-solid border-2"
             handleClick={account ? logout : requestAccount}
           />
-          {account && (
-            <div className="py-3 px-5 flex items-center text-white rounded-full bg-primary-blue min-w-[130px]  gap-2">
-              <div className="w-3 h-3 bg-green-500 rounded-full"></div>
-              <p>Connected</p>
-            </div>
+          {showProfile && (
+            <>
+              <div className="py-3 px-5 flex items-center text-white rounded-full bg-primary-blue min-w-[130px]  gap-2">
+                <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+                <p>Connected</p>
+              </div>
+              <div className="py-3 px-5 flex items-center text-primary-blue rounded-full bg-white min-w-[130px]  gap-2">
+                {account && (
+                  <Avatar
+                    name={account}
+                    size="20"
+                    round={true}
+                    color="orange"
+                  />
+                )}
+                <p>{`${account?.substring(0, 6)}....${account?.substring(
+                  account.length - 4
+                )}`}</p>
+              </div>
+            </>
           )}
         </div>
       </nav>
