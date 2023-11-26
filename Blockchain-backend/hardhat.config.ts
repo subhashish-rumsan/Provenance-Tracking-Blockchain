de0def3bbbffc6514756a4fc2425e1d6aef8e68b
Subhashish Jung Shah (Rumsan) <70791189+subhashishjungshah@users.noreply.github.com>
2023-11-26 17:00:10 +0545

    Feature login contract (#5)
    * creating a framework for further work
    
    * added new login contract
    
    * added auth contract for our app
    
    ---------
    
    Co-authored-by: Subhashish Jung Shah <subhashishjungshah@Subhashishs-MacBook-Pro.local>

--- Blockchain-backend/hardhat.config.ts
+++ Blockchain-backend/hardhat.config.ts
@@ -1,8 +1,19 @@
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const INFURA_API_KEY = process.env.INFURA_PRIVATE_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY as string;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
