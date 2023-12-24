b47e0d6e39fec06148002fdc4ecd9b7b27bf9e41
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2023-12-24 20:19:18 +0545

    feat: create contract for Car

---
+++ Blockchain-backend/contracts/CarShare.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { ICarShare } from "../interfaces/ICarShare.sol";
+
+contract CarShare is ERC20, ICarShare{
+    uint256 private totalShare = 100;
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+
+    function mint (address owner) external {
+        _mint(owner, totalShare);
+    }
+
+    function transferToken(address from, address to, uint256 value) external {
+        _transfer(from, to, value);
+    }
+     
+    function getShare(address user) external view returns (uint256) {
+        return balanceOf(user);
+    }
+    
+    function getTotalShare() external view  returns (uint256){
+       return totalSupply();
+    }
+
+    function getTokens() external view  returns (string memory, string memory){
+        return (name(), symbol());
+    }
+}
---
+++ Blockchain-backend/interfaces/ICarShare.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+interface ICarShare {
+   function mint(address owner) external;
+   function transferToken(address from, address to, uint256 value) external ;
+   function getShare(address user) external view  returns (uint256);
+    function getTotalShare() external view  returns (uint256);
+    function getTokens() external view  returns (string memory, string memory);
+}
