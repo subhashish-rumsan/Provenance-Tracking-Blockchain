7fab86edb63d7129a9014682f7ebc06b60c75e6d
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2023-12-25 18:08:39 +0545

    feat: add contract verification script

--- Blockchain-backend/README.md
+++ Blockchain-backend/README.md
@@ -11,3 +11,21 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.ts
 ```
+
+## To compile the contract
+
+```shell
+npx hardhat compile
+```
+
+## To deploy the contract
+
+```shell
+npx hardhat run scripts/deploy.ts --network sepolia
+```
+
+## To verify the deployed contract
+
+```shell
+npx hardhat verify --network sepolia contract address
+```
--- Blockchain-backend/hardhat.config.ts
+++ Blockchain-backend/hardhat.config.ts
@@ -1,19 +1,28 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+// import "@nomiclabs/hardhat-etherscan";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const INFURA_API_KEY = process.env.INFURA_PRIVATE_KEY;
-const SEPOLIA_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY as string;
+// const INFURA_API_KEY = process.env.INFURA_PRIVATE_KEY;
+// const SEPOLIA_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY as string;
+const INFURA_API_KEY = "15c8394ce1a247dc94dde661a538574d";
+const SEPOLIA_PRIVATE_KEY =
+  "9bed18a7b4bca37a79549047af7dd10d6e4cce4ca8f7833daf34f282c4db2f4d";
+const ETHSCAN_API_KEY = "MJZMBRI2JYHQ3P3KW2481GZ5IIVYMDXR59";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  defaultNetwork: "sepolia",
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: ETHSCAN_API_KEY,
   },
 };
 
